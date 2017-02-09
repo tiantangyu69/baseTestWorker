61a4c169d2a11c945b9d0972cdf60f9a3eb38a91
bjlizhitao <bjlizhitao@corp.netease.com>
2017-02-09 12:44:15 +0800

    测试动态代理

--- src/main/java/cc/lee/java/lang/TestProxyClass.java
+++ src/main/java/cc/lee/java/lang/TestProxyClass.java
@@ -1,5 +1,8 @@
 package cc.lee.java.lang;
 
+import sun.misc.ProxyGenerator;
+
+import java.io.FileOutputStream;
 import java.lang.reflect.Proxy;
 
 /**
@@ -13,5 +16,19 @@ public class TestProxyClass implements TestProxyInterface {
     public static void main(String[] args) {
         TestProxyInterface proxy = (TestProxyInterface) Proxy.newProxyInstance(TestProxyClass.class.getClassLoader(), new Class[]{TestProxyInterface.class}, new TestInvocationHandler(new TestProxyClass()));
         proxy.say();
+
+        createProxyClassFile();
+    }
+
+    public static void createProxyClassFile() {
+        String name = "TestProxyClass";
+        byte[] data = ProxyGenerator.generateProxyClass(name, new Class[]{TestProxyClass.class});
+        try {
+            FileOutputStream out = new FileOutputStream(name + ".class");
+            out.write(data);
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
